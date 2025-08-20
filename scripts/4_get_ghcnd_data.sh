f648ce1575dc9da394549a9555eb49c71d005706
Mirela Balan <mire.balan@gmail.com>
2025-08-20 09:09:31 +0200

    lower line requirement in 8_concatenate_dly.sh to prevent GitHub Action crash

--- scripts/4_get_ghcnd_data.sh
+++ scripts/4_get_ghcnd_data.sh
@@ -6,6 +6,7 @@
 ## we can use the same script to download all the files
 
 file=$1
+rm data/$file
 ## download these files to the "data/" directory
 ## "-nc" only redownload next time if there is a newer file version than what is downloaded
 wget -nc -P data/  https://www.ncei.noaa.gov/pub/data/ghcn/daily/$file
